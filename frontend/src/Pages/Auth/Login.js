a7b5b9018bf241c715852551fb8c1a4b46e4452b
fightforfreedom2k3 <nhhoang15122003@gmail.com>
2024-04-04 11:23:52 +0700

    update

--- frontend/src/Pages/Auth/Login.js
+++ frontend/src/Pages/Auth/Login.js
@@ -1,60 +1,63 @@
-import "../Auth/Login.css"
+import "../Auth/Logger.css"
+import loggerImg from "../../Assets/Logger.png"
 function Login() {
     return ( 
-<div class="container">
-    <div class="ad-block"></div>
+        <div class="container">
+            {/* ảnh nền */}
+            <div class="ad-block">
+                <img src={loggerImg}></img>
+            </div>
+            {/* phần đăng kí */}
+            <div class="logger">
+                <h1>Đăng nhập</h1>
+                {/* form đăng kí */}
+                <div class="internal login">
+                    <form method="post" action="">
+                        {/* tài khoản */}
+                        <div class="row">
+                            <div class="label">Tài khoản</div>
+                            <div class="input">
+                                <input type="text" name="UserName" placeholder="Số điện thoại hoặc Email..." id="UserName"></input>
+                            </div>
+                        </div>
+                        {/* mật khẩu */}
+                        <div class="row">
+                            <div class="label">Mật khẩu</div>
+                            <div class="input">
+                                <input type="password" name="UserPassword" placeholder="*************" id="UserPassword"></input>
+                            </div>
+                        </div>
+                        {/* nút đăng nhập */}
+                        <div class="row">
+                            <button class="login-btn" type="submit">Đăng nhập</button>
+                        </div>
 
-    <div class="login">
-        <h1 class="title">Đăng nhập</h1>
-        {/*Đăng nhập bằng tài khoản*/}
-        <div class="internal">
-            <form method="post" action="">
-                
-                <div class="row">
-                    <div class="label">Tài khoản</div>
-                    <div class="input">
-                        <input type="text" name="UserName" placeholder="Số điện thoại hoặc Email..." id="UserName"></input>
-                    </div>
+                    </form>
                 </div>
-                
-                <div class="row">
-                    <div class="label">Mật khẩu</div>
-                    <div class="input">
-                        <input type="password" name="UserPassword" placeholder="*************" id="UserPassword"></input>
-                    </div>
-                </div>
-
-                <div class="button">
-                    <button class="login-btn" type="submit">Đăng nhập</button>
-                </div>
-
+                {/* Hoặc */}
                 <div class="break-line">
-                    <span class="break-caption">Hoặc</span>
+                    <em class="break-caption">Hoặc</em>
                 </div>
-            </form>
-        </div>
-            
-    {/* Đăng nhập bằng liên kết */}
-        <div class="external">
-            <form method="post">
-                <div class="button">
-                    <button class="login-ex" title="Đăng nhập qua Facebook" type="submit" id="Facebook">Đăng nhập bằng Facebook</button>
+                {/* --------------------------------------- */}
+                {/* Đăng nhập bằng liên kết */}
+                <div class="external">
+                    <form method="post">
+                        <div class="button">
+                            <button class="logger-external" title="Đăng nhập qua Facebook" type="submit" id="Facebook">Đăng nhập bằng Facebook</button>
+                        </div>
+
+                        <div class="button">
+                            <button class="logger-external" title="Đăng nhập qua Google" type="submit" id="Google">Đăng nhập bằng Google</button>
+                        </div>
+                    </form>
                 </div>
-
-                <div class="button">
-                    <button class="login-ex" title="Đăng nhập qua Google" type="submit" id="Google">Đăng nhập bằng Google</button>
+                {/* Chuyển hướng đăng nhập */}
+                <div class="other">
+                    <p class="note">Chưa có tài khoản?</p>
+                    <a class="nav" href="../register">Đăng kí</a>
                 </div>
-
-            </form>
-        </div>
-
-        <div class="for-register">
-            <p class="register-note">Chưa có tài khoản?</p>
-            <a class="register-btn" href="../register">Đăng kí</a>
-        </div>
-
-    </div>
-</div>
+            </div>
+        </div>      
      );
 }
 
--- frontend/src/Pages/Auth/Register.js
+++ frontend/src/Pages/Auth/Register.js
@@ -1,6 +1,70 @@
+import "../Auth/Logger.css"
+import loggerImg from "../../Assets/Logger.png"
 function Register() {
     return ( 
-        <h1>Register</h1>
+        <div class="container">
+            {/* ảnh nền */}
+            <div class="ad-block">
+                <img src={loggerImg}></img>
+            </div>
+            {/* phần đăng kí */}
+            <div class="logger">
+                <h1>Đăng kí</h1>
+                {/* form đăng kí */}
+                <div class="internal">
+                    <form method="post" action="">
+                        {/* tài khoản */}
+                        <div class="row">
+                            <div class="label">Tài khoản</div>
+                            <div class="input">
+                                <input type="text" name="UserName" placeholder="Số điện thoại hoặc Email..." id="UserName"></input>
+                            </div>
+                        </div>
+                        {/* mật khẩu */}
+                        <div class="row">
+                            <div class="label">Mật khẩu</div>
+                            <div class="input">
+                                <input type="password" name="UserPassword" placeholder="*************" id="UserPassword"></input>
+                            </div>
+                        </div>
+                        {/* xác nhận mật khẩu */}
+                        <div class="row">
+                            <div class="label">Nhập lại mật khẩu</div>
+                            <div class="input">
+                                <input type="password" name="ConfirmUserPassword" placeholder="*************" id="ConfirmUserPassword"></input>
+                            </div>
+                        </div>
+                        {/* nút đăng kí */}
+                        <div class="row">
+                            <button class="logger-btn" type="submit">Đăng kí</button>
+                        </div>
+
+                    </form>
+                </div>
+                {/* Hoặc */}
+                <div class="break-line">
+                    <em class="break-caption">Hoặc</em>
+                </div>
+                {/* --------------------------------------- */}
+                {/* Đăng nhập bằng liên kết */}
+                <div class="external">
+                    <form method="post">
+                        <div class="button">
+                            <button class="logger-external" title="Đăng nhập qua Facebook" type="submit" id="Facebook">Đăng nhập bằng Facebook</button>
+                        </div>
+
+                        <div class="button">
+                            <button class="logger-external" title="Đăng nhập qua Google" type="submit" id="Google">Đăng nhập bằng Google</button>
+                        </div>
+                    </form>
+                </div>
+                {/* Chuyển hướng đăng nhập */}
+                <div class="other">
+                    <p class="note">Đã có tài khoản?</p>
+                    <a class="nav" href="../login">Đăng nhập</a>
+                </div>
+            </div>
+        </div>        
      );
 }
 
