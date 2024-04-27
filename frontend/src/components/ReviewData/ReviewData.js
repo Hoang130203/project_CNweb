44062cf5a9032d11e179cebfe5f7dda666dec5c4
Your Name <you@example.com>
2024-04-27 22:20:34 +0700

    Product Review

---
+++ frontend/src/components/ReviewData/ReviewData.js
@@ -0,0 +1,95 @@
+const review = [
+   {
+     "id": 1,
+     "name": "Nguyen Van A1",
+     "accountImage" :"",
+     "time": "2024-05-15",
+     "comment": "Máy gọn nhẹ, chạy lâu không bị nóng, nhỏ nhắn, dễ cầm",
+     "commentImage": "",
+     "rate": 4
+   },
+   {
+     "id": 2,
+     "name": "Nguyen Van B2",
+     "accountImage" :"",
+     "time": "2024-05-16",
+     "comment": "Sản phẩm rất tốt, đáng đồng tiền bát gạo",
+     "commentImage": "",
+     "rate": 5
+   },
+   {
+     "id": 3,
+     "name": "Nguyen Van C3",
+     "accountImage" :"",
+     "time": "2024-05-17",
+     "comment": "Thiết kế đẹp, màu sắc bắt mắt, dễ sử dụng",
+     "commentImage": "",
+     "rate": 3
+   },
+   {
+     "id": 4,
+     "name": "Nguyen Van D4",
+     "accountImage" :"",
+     "time": "2024-05-18",
+     "comment": "Đóng gói cẩn thận, giao hàng nhanh chóng",
+     "commentImage": "",
+     "rate": 4
+   },
+   {
+     "id": 5,
+     "name": "Nguyen Van E5",
+     "accountImage" :"",
+     "time": "2024-05-19",
+     "comment": "Chất lượng sản phẩm tuyệt vời, giá thành hợp lý",
+     "commentImage": "",
+     "rate": 5
+   },
+   {
+     "id": 6,
+     "name": "Nguyen Van F6",
+     "accountImage" :"",
+     "time": "2024-05-20",
+     "comment": "Máy chạy mượt, không gặp lỗi phần mềm",
+     "commentImage": "",
+     "rate": 4
+   },
+   {
+     "id": 7,
+     "name": "Nguyen Van G7",
+     "accountImage" :"",
+     "time": "2024-05-21",
+     "comment": "Bảo hành nhanh chóng, phục vụ tận tình",
+     "commentImage": "",
+     "rate": 3
+   },
+   {
+     "id": 8,
+     "name": "Nguyen Van H8",
+     "accountImage" :"",
+     "time": "2024-05-22",
+     "comment": "Pin sạc nhanh, sử dụng lâu",
+     "commentImage": "",
+     "rate": 5
+   },
+   {
+     "id": 9,
+     "name": "Nguyen Van I9",
+     "accountImage" :"",
+     "time": "2024-05-23",
+     "comment": "Đáng đồng tiền bát gạo, không thất vọng",
+     "commentImage": "",
+     "rate": 2
+   },
+   {
+     "id": 10,
+     "name": "Nguyen Van J10",
+     "accountImage" :"",
+     "time": "2024-05-24",
+     "comment": "Thiết kế sang trọng, màn hình sắc nét",
+     "commentImage": "",
+     "rate": 5
+   }
+ ];
+ 
+
+export default review
