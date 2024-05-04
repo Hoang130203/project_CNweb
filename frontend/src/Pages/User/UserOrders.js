838ada860a6c205d0d0b092a19b988129e36516b
Your Name <you@example.com>
2024-05-04 22:47:50 +0700

    purchase order form

--- frontend/src/Pages/User/UserOrders.js
+++ frontend/src/Pages/User/UserOrders.js
@@ -1,7 +1,153 @@
-import React from 'react'
+import React from 'react';
+import styles from './User.module.scss';
+import classNames from 'classnames/bind';
+import products from '../../components/ProductData/ProductData';
+import { Link } from 'react-router-dom';
+import { useState } from 'react';
+
+const cx = classNames.bind(styles);
+
+function TotalMoney() {
+
+  const product = products[0]
+  const formatPrice = (price) => {
+    const formattedPrice = Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
+    return formattedPrice;
+  };
+
+  return (
+    <div>
+      <div style={{display: 'flex', flexDirection: 'column', alignItems: 'flex-end'}}>
+        <div style={{display: 'flex', height: '35px'}}>
+          <p style={{marginTop: '18px'}}>Phí vận chuyển:</p>
+          <span style={{ marginRight: '5px' }}></span>
+          <p style={{}} className={cx('new__price')}>{formatPrice(product.newPrice)}</p>
+        </div>
+        <div style={{display: 'flex', height: '35px'}}>
+          <p style={{marginTop: '18px'}}>Thành tiền:</p>
+          <span style={{ marginRight: '5px' }}></span>
+          <p style={{}} className={cx('new__price')}>{formatPrice(product.newPrice)}</p>
+        </div>
+
+        <div style={{marginTop: '18px'}}>
+          <button style={{border: 'none', margin: '0px 0px 0px 0px', backgroundColor: 'rgb(226, 58, 58)', color:'white', borderRadius: '6px', width:'150px', height: '35px', fontSize: '16px'}}>Hủy đơn hàng</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+function Product() {
+  const product = products[0]
+  const formatPrice = (price) => {
+    const formattedPrice = Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
+    return formattedPrice;
+  };
+
+  return (
+    <div>
+      <div style={{display: 'flex'}}>
+        <div style={{flex: '0.2'}}>
+          <img alt='Product' style={{width: '170px'}} src='https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/l/a/laptop-asus-vivobook-s-14-flip-tp3402va-lz025w-thumbnails.png' />
+        </div>
+        <div style={{flex: '0.95'}}>
+          <p style={{margin: '20px 0px 0px 20px', fontSize: '22px'}}>{product.name}</p>
+          <p style={{margin: '8px 0px 0px 20px', fontSize: '20px'}}>(Newseal)</p>
+          <p style={{margin: '36px 0px 0px 20px', fontSize: '20px'}}>x2</p>
+        </div>
+        <div style={{flex: '0.55'}}>
+          <div className={cx('price__container')} style={{paddingRight:'0%', display: 'flex', paddingTop: '40%'}}>
+            {product.oldPrice && <p style={{}} className={cx('old__price')}>{formatPrice(product.oldPrice)}</p>}
+            <p style={{}} className={cx('new__price')}>{formatPrice(product.newPrice)}</p>
+          </div>
+        </div>
+      </div>
+      <div style={{margin: '15px 0px 0px 0px'}}>
+        <button style={{border: 'none', margin: '0px 0px 0px 520px', backgroundColor: 'rgb(226, 58, 58)', color:'white', borderRadius: '6px', width:'150px', height: '35px', fontSize: '16px'}}>Đánh giá</button>
+        <button style={{border: 'none', margin: '0px 0px 0px 10px', backgroundColor: 'rgb(81, 191, 228)', color:'white', borderRadius: '6px', width:'150px', height: '35px', fontSize: '16px'}}>Bình luận</button>
+      </div>
+
+      <hr style={{ borderTop: '2px solid #B6B6B6', width: 'auto', margin: '18px 0px 0px 0px' }} />
+    </div>
+  )
+}
+
+function Order() {
+
+  const s = "Thanh toán khi nhận hàng"
+
+  return (
+    <div className={cx('order')}>
+      <div style={{display: 'flex'}}>
+        <div style={{display: 'flex', flex: '1'}}>
+          <p style={{display: 'flex', flex: '0.06'}}>#1</p>
+          <p style={{display: 'flex', flex: '0.31'}}>Hình thức thanh toán: </p>
+          <p style={{display: 'flex', flex: '1', color: 'blue'}}>{s}</p>
+        </div>
+        <div style={{display: 'flex', justifyContent: 'space-between' }}>
+          <p style={{}}>Trạng thái:</p>
+          <span style={{ marginRight: '5px' }}></span>
+          <p style={{color: '#888080'}}>Chờ xác nhận</p>
+        </div>
+      </div>
+
+      <hr style={{ borderTop: '2px solid #B6B6B6', width: 'auto', margin: '18px 0px 0px 0px' }} />
+
+      <Product />
+      <Product />
+      <TotalMoney />
+    </div>
+  )
+}
 
 export default function UserOrders() {
+
+  const [selectedLink, setSelectedLink] = useState('all');
+
+  const handleLinkClick = (link) => {
+    setSelectedLink(link);
+  };
+
   return (
-    <div>UserOrders</div>
+    <div className={cx('userOrder')}>
+      {/* Title */}
+      <div className={cx('title')}>
+        <Link
+          style={{ textDecoration: selectedLink === 'all' ? 'underline' : 'none', color: selectedLink === 'all' ? '#D37B29' : 'black', fontSize: '22px' }}
+          onClick={() => handleLinkClick('all')}
+        >
+          Tất cả
+        </Link>
+        <Link
+          style={{ textDecoration: selectedLink === 'unconfirmed' ? 'underline' : 'none', color: selectedLink === 'unconfirmed' ? '#D37B29' : 'black', fontSize: '22px' }}
+          onClick={() => handleLinkClick('unconfirmed')}
+        >
+          Chưa xác nhận
+        </Link>
+        <Link
+          style={{ textDecoration: selectedLink === 'shipping' ? 'underline' : 'none', color: selectedLink === 'shipping' ? '#D37B29' : 'black', fontSize: '22px' }}
+          onClick={() => handleLinkClick('shipping')}
+        >
+          Đang vận chuyển
+        </Link>
+        <Link
+          style={{ textDecoration: selectedLink === 'success' ? 'underline' : 'none', color: selectedLink === 'success' ? '#D37B29' : 'black', fontSize: '22px' }}
+          onClick={() => handleLinkClick('success')}
+        >
+          Thành công
+        </Link>
+        <Link
+          style={{ textDecoration: selectedLink === 'cancelled' ? 'underline' : 'none', color: selectedLink === 'cancelled' ? '#D37B29' : 'black', fontSize: '22px' }}
+          onClick={() => handleLinkClick('cancelled')}
+        >
+          Đã hủy
+        </Link>
+      </div>
+
+      <hr style={{ border: '0.1px solid #B6B6B6', width: 'auto', paddingLeft: '4px', margin: '22px 0px 0px 12px' }} />
+
+      <Order />
+      <Order />
+    </div>
   )
 }
--- frontend/src/components/ProductReview/ProductReview.js
+++ frontend/src/components/ProductReview/ProductReview.js
@@ -9,85 +9,126 @@ import { IoIosStar, IoIosStarOutline } from "react-icons/io";
 
 const cx = classNames.bind(styles);
 
+// Comment Component
 function CommentBox () {
     return (
         <div className={cx('commentBox')}>
+            {/* Account Image */}
             <div className={cx('column1')}>
-                <img className={cx('accImg')} src = 'https://s3-alpha-sig.figma.com/img/0e69/55b5/ac6bae2245e3befa7985f1a3d42889b3?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a53ArzCCmnybP0ZdfwRV0M3pBSXSE3kFrjRKaK1HamAz6hv5hie5F7aBRMhsEejpw2xCwE7kFKt9Ruvuf7H2D1zF6hn5LhkvcXlkV-f-uBzXYgIWrlj9vwyTkaNc9LN~G2Y5RxYHXfmB5RoWHwWwVofBJotF9tDEK55gwRyOOaWz3qhXHvSBfor3PMGvGatCO2a4w0Ez5X6fAfGATD8AqdJVuMggYpv7F1DEQCnQSh3wFiDOWT6QCIA0PpJ1XuLFqMmsE2XbPBS3H3MC3mmmV5jpuNvYQWm9Kq90HvoQMN-Y70U4RQrW0IPM4ceDt1z7Hcva6LO9UXfAw4zauP5jQw__' />
+                <img alt='Account' className={cx('accImg')} src = 'https://s3-alpha-sig.figma.com/img/0e69/55b5/ac6bae2245e3befa7985f1a3d42889b3?Expires=1714348800&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4&Signature=a53ArzCCmnybP0ZdfwRV0M3pBSXSE3kFrjRKaK1HamAz6hv5hie5F7aBRMhsEejpw2xCwE7kFKt9Ruvuf7H2D1zF6hn5LhkvcXlkV-f-uBzXYgIWrlj9vwyTkaNc9LN~G2Y5RxYHXfmB5RoWHwWwVofBJotF9tDEK55gwRyOOaWz3qhXHvSBfor3PMGvGatCO2a4w0Ez5X6fAfGATD8AqdJVuMggYpv7F1DEQCnQSh3wFiDOWT6QCIA0PpJ1XuLFqMmsE2XbPBS3H3MC3mmmV5jpuNvYQWm9Kq90HvoQMN-Y70U4RQrW0IPM4ceDt1z7Hcva6LO9UXfAw4zauP5jQw__' />
             </div>
+
             <div className={cx('column2')}>
+                {/* Account Name */}
                 <div className={cx('row1')}>
                     <p className={cx('customerName')}>Lachlan Dempsey</p>
                 </div>
-
+                {/* Time */}
                 <div className={cx('row2')}>
                     <p className={cx('commentTime')}>2023-10-12 11:12</p>
                 </div>
-
+                {/* Comment Text */}
                 <div className={cx('row3')}>
                     <p className={cx('comment')}>Máy khá gọn, thích hợp cho người thích gọn nhẹ dễ bỏ túi</p>
                     <p className={cx('comment')}>Máy nhỏ hơn iPhone XR, cầm chắc tay</p>
                 </div>
-
+                {/* Comment Image */}
                 <div className={cx('row4')}>
-                    <img className={cx('commentImg')} src='https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/l/a/laptop-asus-vivobook-s-14-flip-tp3402va-lz025w-thumbnails.png'/>
-                    <img className={cx('commentImg')} src='https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/l/a/laptop-asus-vivobook-s-14-flip-tp3402va-lz025w-thumbnails.png'/>
+                    <img alt='Product' className={cx('commentImg')} src='https://cdn2.cellphones.com.vn/insecure/rs:fill:358:358/q:90/plain/https://cellphones.com.vn/media/catalog/product/l/a/laptop-asus-vivobook-s-14-flip-tp3402va-lz025w-thumbnails.png'/>
                 </div>
             </div>
         </div>
     )
 }
 
+// Product Rating component
 export function ProductRating() {
 
-    const [star, setStar] = useState(5)
+    const [star1, setStar1] = useState(5)
+    const [star2, setStar2] = useState(5)
+    const [star3, setStar3] = useState(5)
+    const [star4, setStar4] = useState(5)
     const [starState, setStarState] = useState('Excellent')
 
     // Hàm đánh giá số sao
     const handleStar = (index) => {
         
-        setStar(index + 1)
+        setStar1(index + 1)
 
-        if (index == 0) {
+        if (index === 0) {
             setStarState('Bad')
         }
 
-        if (index == 1) {
+        if (index === 1) {
             setStarState('Unsatisfactory')
         }
 
-        if (index == 2) {
+        if (index === 2) {
             setStarState('Average')
         }
 
-        if (index == 3) {
+        if (index === 3) {
             setStarState('Satisfactory')
         }
 
-        if (index == 4) {
+        if (index === 4) {
             setStarState('Excellent')
         }
     }
+
+    const { id } = useParams();
+    const product = products.find((p) => p.id === parseInt(id));
+
+    const formatPrice = (price) => {
+        const formattedPrice = Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(price);
+        return formattedPrice;
+    };
 
     return (
         <div className={cx('productRating')}>
             <h2>Product Rating</h2>
             {/* Thông tin sản phẩm */}
-            <div>
+            <div className={cx('productInformation')}>
+                <div className={cx('productImg')} >
+                    <img className={cx('accImg')} alt='product' src={product.image}/>
+                </div>
 
+                <div className={cx('productInfo')}>
+                    <div style={{}}>
+                        <p style={{fontSize: '25px', fontWeight: 'bold'}}>{product.name}</p>
+                    </div>
+                    <div style={{display: 'flex', marginTop: '10px'}}>
+                        <p className={cx('category')}>Category:</p>
+                        <p className={cx('category')} style={{marginLeft: '10px'}}>Product Type</p>
+                    </div>
+                    
+                    <div style={{display: 'flex', marginTop: '10px'}}>
+                        <div className={cx('abc')} style={{display: 'flex'}}>
+                            <p style={{flex: '0.1'}} className={cx('number')}>x</p>
+                            <p style={{flex: '1'}} className={cx('number')}>Product Number</p>
+                            <p style={{flex: '1'}} className={cx('category')}>Product Type</p>
+                        </div>
+                        <div className={cx('price__container')} style={{paddingRight:'0%', display: 'flex'}}>
+                            <div style={{flex:'1.5'}}></div>
+                            {product.oldPrice && <p style={{}} className={cx('old__price')}>{formatPrice(product.oldPrice)}</p>}
+                            <p style={{}} className={cx('new__price')}>{formatPrice(product.newPrice)}</p>
+                        </div>
+                    </div>
+                </div>
+                
             </div>
 
             {/* Đánh giá sản phẩm */}
             <div className={cx('productQuality')}>
                 <p>Product Quality</p>
                 <p className={cx('qualityStar')}>
-                    {[...Array(Math.floor(star))].map((_, index) => (
+                    {[...Array(Math.floor(star1))].map((_, index) => (
                         <IoIosStar key={index} color="#FFCB45" onClick={() => handleStar(index)} style={{ color: "#FFCB45", fontSize: "20px" }}/>
                     ))}
 
-                    {[...Array(Math.floor(5 - star))].map((_, index) => (
+                    {[...Array(Math.floor(5 - star1))].map((_, index) => (
                         <div key={index} style={{ display: "inline-block" }}>
-                            <IoIosStarOutline onClick={() => handleStar(index + star)} style={{ color: "#FFCB45", fontSize: "20px" }} />
+                            <IoIosStarOutline onClick={() => handleStar(index + star1)} style={{ color: "#FFCB45", fontSize: "20px" }} />
                             <IoIosStar style={{ color: "#FFCB45", fontSize: "20px", position: "absolute", zIndex: "-1" }} />
                         </div>
                     ))}
@@ -118,18 +159,14 @@ export function ProductRating() {
                 <div>
                     <p>Dịch vụ của người bán</p>
                     <p>
-                        {[...Array(Math.floor(5))].map((_, index) => (
-                            <IoIosStar key={index} color="#FFCB45" />
+                        {[...Array(Math.floor(star2))].map((_, index) => (
+                            <IoIosStar key={index} color="#FFCB45" onClick={() => setStar2(index + 1)} style={{ color: "#FFCB45", fontSize: "20px" }}/>
                         ))}
-                        {/* {[...Array(5 - Math.floor(5))].map((_, index) => (
-                            <IoIosStar key={index + Math.floor(product.rating)} />
-                        ))} */}
-                    </p>
-                    <p>
-                        {[...Array(Math.floor(5))].map((_, index) => (
+
+                        {[...Array(Math.floor(5 - star2))].map((_, index) => (
                             <div key={index} style={{ display: "inline-block" }}>
-                                <IoIosStarOutline style={{ color: "#FFCB45", fontSize: "24px" }} />
-                                <IoIosStar style={{ color: "#FFCB45", fontSize: "24px", position: "absolute", zIndex: "-1" }} />
+                                <IoIosStarOutline onClick={() => setStar2(index + star2 + 1)} style={{ color: "#FFCB45", fontSize: "20px" }} />
+                                <IoIosStar style={{ color: "#FFCB45", fontSize: "20px", position: "absolute", zIndex: "-1" }} />
                             </div>
                         ))}
                     </p>
@@ -137,23 +174,31 @@ export function ProductRating() {
                 <div>
                     <p>Tốc độ giao hàng</p>
                     <p>
-                        {[...Array(Math.floor(5))].map((_, index) => (
-                            <IoIosStar key={index} color="#FFCB45" />
+                        {[...Array(Math.floor(star3))].map((_, index) => (
+                            <IoIosStar key={index} color="#FFCB45" onClick={() => setStar3(index + 1)} style={{ color: "#FFCB45", fontSize: "20px" }}/>
                         ))}
-                        {/* {[...Array(5 - Math.floor(5))].map((_, index) => (
-                            <IoIosStar key={index + Math.floor(product.rating)} />
-                        ))} */}
+
+                        {[...Array(Math.floor(5 - star3))].map((_, index) => (
+                            <div key={index} style={{ display: "inline-block" }}>
+                                <IoIosStarOutline onClick={() => setStar3(index + star3 + 1)} style={{ color: "#FFCB45", fontSize: "20px" }} />
+                                <IoIosStar style={{ color: "#FFCB45", fontSize: "20px", position: "absolute", zIndex: "-1" }} />
+                            </div>
+                        ))}
                     </p>
                 </div>
                 <div>
                     <p>Tài xế</p>
                     <p>
-                        {[...Array(Math.floor(5))].map((_, index) => (
-                            <IoIosStar key={index} color="#FFCB45" />
+                        {[...Array(Math.floor(star4))].map((_, index) => (
+                            <IoIosStar key={index} color="#FFCB45" onClick={() => setStar4(index + 1)} style={{ color: "#FFCB45", fontSize: "20px" }}/>
                         ))}
-                        {/* {[...Array(5 - Math.floor(5))].map((_, index) => (
-                            <IoIosStar key={index + Math.floor(product.rating)} />
-                        ))} */}
+
+                        {[...Array(Math.floor(5 - star4))].map((_, index) => (
+                            <div key={index} style={{ display: "inline-block" }}>
+                                <IoIosStarOutline onClick={() => setStar4(index + star4 + 1)} style={{ color: "#FFCB45", fontSize: "20px" }} />
+                                <IoIosStar style={{ color: "#FFCB45", fontSize: "20px", position: "absolute", zIndex: "-1" }} />
+                            </div>
+                        ))}
                     </p>
                     <input placeholder='Thêm đánh giá chi tiết'/>
                 </div>
